4e2478bf68f241d207f3d32a96ecf563c5a35b18
Tony <anton-rus@mail.ru>
2020-10-05 23:59:03 +0300

    Рефакторинг, часть 1

--- js/util.js
+++ js/util.js
@@ -1,6 +1,10 @@
 'use strict';
 
 (() => {
+  const WIZARD_COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+  const WIZARD_EYES_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
+  const FIREBALL_COLORS = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
+
   window.util = {
     randomize: (array) => array[Math.floor(Math.random() * array.length)],
     setKeyEvent: (evt, key, action) => {
@@ -8,8 +12,8 @@
         action();
       }
     },
-    wizardCoatColors: [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`],
-    wizardEyesColors: [`black`, `red`, `blue`, `yellow`, `green`],
-    fireballColors: [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`]
+    wizardCoatColors: WIZARD_COAT_COLORS,
+    wizardEyesColors: WIZARD_EYES_COLORS,
+    fireballColors: FIREBALL_COLORS
   };
 })();
